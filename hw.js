f9b0f8762543f3d5967ecf01c1d56b1ca7763939
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-03-12 12:40:11 +0100

    fetch and render
    I have fetched the data but it is not rendering in cards

--- hw.js
+++ hw.js
@@ -1,3 +1,83 @@
+
+/*
 fetch('https://striveschool-api.herokuapp.com/books')
 .then( response => response.json())
 .then(data => console.log(data));
+*/
+/*
+fetch('https://striveschool-api.herokuapp.com/books')
+.then(function(response){
+    return response.json()
+})
+.then(function(books){
+    console.log(books)
+})
+
+*/
+
+/*
+    fetch("https://striveschool-api.herokuapp.com/books") 
+
+
+      .then((response) => {
+        return response.json();
+      })
+      .then((body) => {
+        let cards = document.getElementsByClassName("card");
+  
+       // for (let i = 0; i < cards.length; i++) {
+          //cards[i].firstElementChild.remove(); //removes svg
+          let img = document.createElement("img");
+          img.src = body.photos[i].src.large;
+          img.className = "bd-placeholder-img card-img-top";
+          img.style = "height: 200px; object-fit: cover";
+  
+          cards[i].insertBefore(img, cards[i].firstChild); //adds the new image before other content
+  
+          
+        }
+    */
+
+        fetch('https://striveschool-api.herokuapp.com/books')
+     .then(response => response.json())
+     .then(books => {renderBooks(books.results)
+     });
+
+  const cardsContainer = document.querySelector("#cards-container");
+
+  function renderBooks(books) {
+    books.forEach(books => {
+      const div = document.createElement('div');
+      const image = document.createElement('img');
+      const name = document.createElement('h3');
+      const species = document.createElement('h3');
+      const like = document.createElement('button');
+      const skip = document.createElementNS('button');
+
+      div.classList = 'card'
+      image.classList = 'card-img'
+      like.classList = 'empty'
+
+      image.src = books.image
+      asin.innerText = ` asin: ${books.asin}`
+      title.innerText = `title: ${books.title}`
+      price.innerText = `price: ${books.price}`
+      category.innerText = `category ${books.category}'
+
+     
+      like.textContent = 'like'
+      skip.textContent = 'Skip'
+      
+      div.appendChild(asin)
+      div.appendChild(title)
+      div.appendChild(image)
+      div.appendChild(price)
+      div.appendChild(category)
+      div.appendChild(like)
+      div.appendChild(skip)
+      cardsContainer.appendChild(div)
+      })
+     };
+
+
+
